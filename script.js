24aa2fd8c763fe03d0031b8ce7e2fb6ec383bed1
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-05-20 00:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,56 +1,20 @@
-class PriorityQueue {
-  constructor() {
-    this.collection = [];
-  }
-  
-  enqueue(element){
-    if (this.isEmpty()){ 
-      this.collection.push(element);
-    } else {
-      let added = false;
-      for (let i = 1; i <= this.collection.length; i++){
-        if (element[1] < this.collection[i-1][1]){ 
-          this.collection.splice(i-1, 0, element);
-          added = true;
-          break;
-        }
-      }
-      if (!added){
-        this.collection.push(element);
-      }
-    }
-  };
-  
-  dequeue() {
-    let value = this.collection.shift();
-    return value;
-  };
-  
-  isEmpty() {
-    return (this.collection.length === 0) 
-  };
-  
-  size(){
-    return this.collection.length;
-  }
-}
+
 function mincost(ropes)
 { 
 //write your code here
 // return the min cost
-  let pq = new PriorityQueue();
-    for (let i = 0; i < ropes.length; i++) {
-        pq.enqueue(ropes[i]);
-    }
+	let minHeap = new MinHeap();
+	for (let i = 0; i < ropes.length; i++) {
+	    minHeap.insert(ropes[i]);
+	}
 
-    let totalCost = 0;
-    while (pq.size() > 1) {
-        let cost = pq.dequeue() + pq.dequeue();
-        totalCost += cost;
-        pq.enqueue(cost);
-    }
-
-    return totalCost;
+	let totalCost = 0;
+	while (minHeap.size() > 1) {
+	    let cost = minHeap.remove() + minHeap.remove();
+	    totalCost += cost;
+	    minHeap.insert(cost);
+	}
+	return totalCost;
 }
 
 module.exports=mincost;
