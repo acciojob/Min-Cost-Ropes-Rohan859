eff2e63587b6137f70a220192f45ae3cbc05d3c6
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-05-19 23:48:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function mincost(arr)
+function mincost(ropes)
 { 
 //write your code here
 // return the min cost
-  
+  let pq = new PriorityQueue();
+    for (let i = 0; i < ropes.length; i++) {
+        pq.enqueue(ropes[i]);
+    }
+
+    let totalCost = 0;
+    while (pq.size() > 1) {
+        let cost = pq.dequeue() + pq.dequeue();
+        totalCost += cost;
+        pq.enqueue(cost);
+    }
+
+    return totalCost;
 }
 
 module.exports=mincost;
