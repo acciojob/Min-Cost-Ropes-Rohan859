b60836df29f668926dc13db89a5ec373bdadc62f
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-05-20 00:29:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+class PriorityQueue {
+  constructor() {
+    this.collection = [];
+  }
+  
+  enqueue(element){
+    if (this.isEmpty()){ 
+      this.collection.push(element);
+    } else {
+      let added = false;
+      for (let i = 1; i <= this.collection.length; i++){
+        if (element[1] < this.collection[i-1][1]){ 
+          this.collection.splice(i-1, 0, element);
+          added = true;
+          break;
+        }
+      }
+      if (!added){
+        this.collection.push(element);
+      }
+    }
+  };
+  
+  dequeue() {
+    let value = this.collection.shift();
+    return value;
+  };
+  
+  isEmpty() {
+    return (this.collection.length === 0) 
+  };
+  
+  size(){
+    return this.collection.length;
+  }
+}
 function mincost(ropes)
 { 
 //write your code here
